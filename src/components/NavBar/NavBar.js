47d78657582000aae4e19f525e8754e371b81d06
Martin <mlgabotto@gmail.com>
2021-03-23 17:09:58 -0300

    first commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink, NavWrapper } from './NavBarElementes'
+
+const NavBar = () => {
+    return (
+        <>
+            <NavWrapper>
+                <NavLink> NavBar</NavLink>
+
+            </NavWrapper>
+        </>
+    )
+}
+
+export default NavBar
---
+++ src/components/NavBar/NavBarElementes.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+export const NavLink = styled(Link)`
+font-size: 2rem;
+color: white;
+background-color: black;
+font-weight: 300;
+`
+
+export const NavWrapper = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: green;
+padding: 20px 0 20px 0
+`
